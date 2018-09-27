2d3ab932cf5f49d6e6b9e1074fe908c285b6cde8
wudududu <296525335@qq.com>
2018-09-27 19:47:42 +0800

    新建项目

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <wd-tree />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import wdTree from './components/wd-tree'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    wdTree
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/wd-tree.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tree-container">
+    <ul class="root-ul">
+
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'wd-tree',
+  props: {
+    treeNodes: [
+      {
+        name: 'node1',
+        nodeType: 'parent',
+        childs: [{name: 'node1-1', nodeType: 'child'}, {name: 'node1-2', nodeType: 'parent', childs: [{name: 'node1-2-1', nodeType: 'child'}]}]
+      },
+      {
+        name: 'node2',
+        nodeType: 'child'
+      }
+    ]
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
